8feffc311c5eff2f27c712fa124eaabf0da4d253
arthurtruong <arthurtruong.work@gmail.com>
2018-09-21 00:58:08 +1000

    first commit

---
+++ redux-example/src/components/Posts.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Posts extends Component {
+    componentWillMount() {
+        console.log(123);
+    }
+  render() {
+    return (
+     <h1>Posts</h1>
+    )
+  }
+}
+
+export default Posts;
