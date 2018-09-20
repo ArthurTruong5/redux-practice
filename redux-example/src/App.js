fd3a3f74900385d482d421b1c006dd65a00a739a
arthurtruong <arthurtruong.work@gmail.com>
2018-09-21 02:09:47 +1000

    react implemented

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Posts from './components/Posts';
+import Postform from './components/Postform';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Postform />
         <Posts />
       </div>
     );
